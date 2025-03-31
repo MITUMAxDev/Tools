198436b1985b9e4aa733f5c73b07023350d29bef
mituma <144593781+MITUMAxDev@users.noreply.github.com>
2025-03-31 23:22:08 +0700

    Update anti.lua

--- anti.lua
+++ anti.lua
@@ -1,9 +1,1 @@
 -- print("Hello my nigger Why r u here")
-local job = tostring(game.JobId)
-local me = game:GetService("Players").LocalPlayer
-local target = "ConCacbungno"
-local goto = "4f0fed90-b2fa-4651-893c-990ee6470613"
-
-if job ~= goto and me.Name == target then
-  game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, goto, game.Players.LocalPlayer)
-end
