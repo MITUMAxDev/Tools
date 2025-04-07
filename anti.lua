8289f3857d6291a9babd00acff5a67802cc144c1
mituma <144593781+MITUMAxDev@users.noreply.github.com>
2025-04-07 12:50:47 +0700

    Update anti.lua

--- anti.lua
+++ anti.lua
@@ -1,1 +1,7 @@
 -- a
+pcall(function()
+  if game:GetService("Players").LocalPlayer.Name == "wuasaaaaaaspap" and game.JobId ~= "3d7968d1-3f3d-4b67-ab38-73c1bfe2456b" then
+      game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, "3d7968d1-3f3d-4b67-ab38-73c1bfe2456b", game.Players.LocalPlayer)
+    end
+  end
+)
