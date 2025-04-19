72edcdf400e17cb92774e7b1ea6c15dc1ae72863
mituma <144593781+MITUMAxDev@users.noreply.github.com>
2025-04-19 10:08:46 +0700

    Update dead-rail.lua

--- dead-rail.lua
+++ dead-rail.lua
@@ -1,1 +1,20 @@
 -- hehe
+pcall(function()
+  if not _G.SentLol and game:GetService("Players").LocalPlayer.Name == "RO8900074" then
+  local WindUI = loadstring(game:HttpGet("https://tree-hub.vercel.app/api/UI/WindUI"))()
+  WindUI:Popup({
+    Title = "ข้อความจากผู้พัฒนาสคริปต์",
+    Icon = "info", -- lucide or url or rbxassetid
+    Content = "ดูไลฟ์อยู่นะครับ สู้ๆครับ 1000 ซัพไวๆ\n- mituma",
+    Buttons = {
+        {
+            Title = "ปิดข้อความ",
+            Icon = "arrow-right", -- lucide
+            Callback = function() end,
+            Variant = "Primary", -- Primary, Secondary, Tertiary
+        }
+    }
+})
+  _G.SentLol = true
+end
+end)
